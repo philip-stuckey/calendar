86fba848b3ef07232f919cedbd252dcf7442d71e
philip-stuckey <developer.philip_stuckey@fastmail.com>
2023-06-03 19:24:48 -0400

    renamed database to _database to hide it from fire

--- src/__main__.py
+++ src/__main__.py
@@ -12,28 +12,28 @@ from database import DataBase
 class App:
 
     def __init__(self, cal_path="~/Calendar/calendar", color=True):
-        self.database = DataBase(Path(cal_path).expanduser())
+        self._database = DataBase(Path(cal_path).expanduser())
         self.color=color
 
     def year(self, year=_date.today().year):
         '''
         show the monthly calendar for each month of the whole year
         '''
-        event_dates = list(map(attrgetter('date'), self.database.year(year)))
+        event_dates = list(map(attrgetter('date'), self._database.year(year)))
         print(YearGrid(year, events=event_dates, color=self.color))
 
     def month(self, month=_date.today().month, year=_date.today().year):
         '''
         show the monthly calendar, today is highlighted green, each day with an event is highlighted orange
         '''
-        event_dates = list(map(attrgetter('date'), self.database.month(year,
+        event_dates = list(map(attrgetter('date'), self._database.month(year,
             month)))
         print(MonthGrid(year, month, events=event_dates, color=self.color))
 
     def week(self, today=None):
         today = _date.today() if today is None else _date.fromisoformat(today)
         w = max(map(len, calendar.day_name))
-        for event in self.database.weekof(today):
+        for event in self._database.weekof(today):
             print(
                 calendar.day_name[event.date.weekday()].ljust(w), 
                 event.time.isoformat() if event.time is not None else ' '*8, 
@@ -60,14 +60,14 @@ class App:
     def day(self, date=None):
         date = _date.fromisoformat(date) if date is not None else _date.today()
         print(date.isoformat())
-        for event in self.database.day(date):
+        for event in self._database.day(date):
             print(event._time_str(), event.description)
 
     def list(self):
         '''
         list upcoming events in chronological order
         '''
-        for event in self.database.list():
+        for event in self._database.list():
             print(event)
 
 if __name__ == '__main__':
