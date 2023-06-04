f867965abd38d9fc0321148bdd611f69ba2a8991
philip-stuckey <developer.philip_stuckey@fastmail.com>
2023-06-04 16:44:47 -0400

    added recurrance parsing and moved recurrance out of calendar file

--- src/events.py
+++ src/events.py
@@ -1,21 +1,11 @@
 import datetime
 from dataclasses import dataclass
-from grammar import line_format
 
 @dataclass
 class Event:
     date: datetime.date
     time: datetime.time
     description: str
-
-    @staticmethod
-    def from_line(line: str):
-        (date,time,description) = line_format.parse(line)
-        return Event(
-                date=date,
-                time=time,
-                description=description.strip()
-        )
 
     def _time_str(self):
         return '' if self.time is None else (' ' + self.time.isoformat())
--- src/grammar.py
+++ src/grammar.py
@@ -2,7 +2,7 @@ from parsy import regex, seq, any_char, string
 import datetime
 
 dash = string('-')
-
+digits=regex('\d+').map(int)
 digits2 = regex('\d\d').map(int)
 
 date_fmt=seq(
@@ -20,7 +20,30 @@ time_fmt=seq(
 
 timesep = string(' ')
 fieldsep = string('\t')
-description=any_char.many().concat()
-line_format = seq(date_fmt, (timesep>>time_fmt).optional(), fieldsep>>description)
 
+import events
+description=any_char.many().concat().map(str.strip)
+event_format = seq(
+        date=date_fmt, 
+        time=(timesep>>time_fmt).optional(), 
+        description=fieldsep>>description
+).combine_dict(events.Event)
 
+# 1995-05-15 12m Robert's Birthday
+# 2023-06-14  7d 18:30 D&D
+# 2020-02-29 48m leap day
+import recurrance
+
+day_period=(digits<<string('d')).map(recurrance.DayPeriod)
+month_period=(digits<<string('m')).map(recurrance.MonthPeriod)
+period = day_period | month_period
+
+recurrance_format = seq(
+        start_date=date_fmt,
+        time=(timesep>>time_fmt).optional(),
+        period=timesep>>period,
+        end_date=date_fmt.optional(default=datetime.date.max),
+        description=fieldsep>>description
+).combine_dict(recurrance.RecurringEvents)
+
+line_format = event_format | recurrance_format
